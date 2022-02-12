d5b987f091519b5ff40438ff8ac34b0d7c4f04c1
krishnaanujan <ka2602@columbia.edu>
2022-02-12 12:27:40 -0500

    created new file to explore Saha dataset

---
+++ code_D2_SahaTilakDavidar_explore.R
@@ -0,0 +1,5 @@
+library(readxl)
+D2_SahaTilakDavidar_Andaman1992_3_data <- read_excel("D2_SahaTilakDavidar_Andaman1992-3_data.xlsx")
+SahaTilakDavidar_Andaman1992_3_metadata <- read_excel("SahaTilakDavidar_Andaman1992-3_metadata.xlsx")
+New names:
+
